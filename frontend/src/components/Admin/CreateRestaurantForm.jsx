c2aea4c96ef4daa74144fd0a0a90c471b410c91a
SAIKO9X <cardosed3@gmail.com>
2025-08-13 15:02:15 -0300

    feat(restaurant): Adiciona validação ao formulário de criação
    Implementa a validação de campos no formulário de criação de restaurante utilizando a biblioteca Yup.
    
    - Define campos obrigatórios como nome, descrição, endereço e contatos.
    - Adiciona regras de formato para email e URLs de redes sociais.
    - Exibe mensagens de erro claras para o utilizador, melhorando a experiência de preenchimento do formulário.

--- frontend/src/components/Admin/CreateRestaurantForm.jsx
+++ frontend/src/components/Admin/CreateRestaurantForm.jsx
@@ -5,6 +5,7 @@ import {
   Grid,
   IconButton,
   TextField,
+  Typography,
 } from "@mui/material";
 import { useFormik } from "formik";
 import { useState } from "react";
@@ -12,6 +13,34 @@ import { uploadImageToCloudinary } from "../../config/UploadToCloudinary";
 import { useDispatch } from "react-redux";
 import { createRestaurant } from "../../state/Restaurant/Action";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup"; // Importar o Yup
+
+// 1. Definir o Esquema de Validação
+const validationSchema = Yup.object({
+  name: Yup.string().required("O nome do restaurante é obrigatório."),
+  description: Yup.string()
+    .required("A descrição é obrigatória.")
+    .min(10, "A descrição deve ter pelo menos 10 caracteres."),
+  cuisineType: Yup.string().required("O tipo de cozinha é obrigatório."),
+  streetAddress: Yup.string().required("O endereço é obrigatório."),
+  stateProvince: Yup.string().required("O estado é obrigatório."),
+  postalCode: Yup.string().required("O código postal é obrigatório."),
+  city: Yup.string().required("A cidade é obrigatória."),
+  country: Yup.string().required("O país é obrigatório."),
+  email: Yup.string()
+    .email("O formato do email é inválido.")
+    .required("O email de contato é obrigatório."),
+  mobile: Yup.string().required("O número de celular é obrigatório."),
+  openingHours: Yup.string().required(
+    "O horário de funcionamento é obrigatório."
+  ),
+  images: Yup.array()
+    .min(1, "É necessário pelo menos uma imagem.")
+    .required("As imagens são obrigatórias."),
+  twitter: Yup.string().url("Por favor, insira uma URL válida."),
+  instagram: Yup.string().url("Por favor, insira uma URL válida."),
+  facebook: Yup.string().url("Por favor, insira uma URL válida."),
+});
 
 const initialValues = {
   name: "",
@@ -72,14 +101,13 @@ export const CreateRestaurantForm = () => {
       images: values.images,
     };
 
-    console.log("data:", data);
-    dispatch(createRestaurant(data, navigate)).then((response) => {
-      console.log("Resposta da criação do restaurante:", response);
-    });
+    dispatch(createRestaurant(data, navigate));
   };
 
   const formik = useFormik({
     initialValues,
+    // 2. Ligar o esquema de validação ao Formik
+    validationSchema: validationSchema,
     onSubmit: handleSubmit,
   });
 
@@ -91,52 +119,59 @@ export const CreateRestaurantForm = () => {
         </h1>
         <form onSubmit={formik.handleSubmit} className="space-y-4">
           <Grid container spacing={2}>
-            <Grid className="flex flex-wrap gap-5" item xs={12}>
-              <input
-                accept="image/*"
-                type="file"
-                id="fileInput"
-                style={{ display: "none" }}
-                onChange={handleImageChange}
-              />
-              <label className="relative" htmlFor="fileInput">
-                <span className="w-24  h-24 cursor-pointer flex items-center justify-center p-3 border rounded-md border-zinc-600">
-                  <AddPhotoAlternate className="text-white" />
-                </span>
-
-                <span>
+            {/* --- Campo de Imagens com Validação --- */}
+            <Grid item xs={12}>
+              <div className="flex flex-wrap gap-5">
+                <input
+                  accept="image/*"
+                  type="file"
+                  id="fileInput"
+                  style={{ display: "none" }}
+                  onChange={handleImageChange}
+                />
+                <label className="relative" htmlFor="fileInput">
+                  <span className="w-24 h-24 cursor-pointer flex items-center justify-center p-3 border rounded-md border-zinc-600">
+                    <AddPhotoAlternate className="text-white" />
+                  </span>
                   {uploadImage && (
                     <div className="absolute left-0 right-0 top-0 bottom-0 w-24 h-24 flex items-center justify-center">
                       <CircularProgress color="secondary" />
                     </div>
                   )}
-                </span>
-              </label>
-
-              <div className="flex flex-wrap gap-2">
-                {formik.values.images.map((image, index) => (
-                  <div key={index} className="relative">
-                    <img
-                      className="w-24 h-24 object-cover"
-                      src={image}
-                      alt="Foto do Restaurante"
-                    />
-                    <IconButton
-                      size="small"
-                      sx={{
-                        position: "absolute",
-                        top: "0",
-                        right: "0",
-                        outline: "none",
-                      }}
-                      onClick={() => handleRemoveImage(index)}
-                    >
-                      <Close sx={{ fontSize: "1rem" }} />
-                    </IconButton>
-                  </div>
-                ))}
+                </label>
+                <div className="flex flex-wrap gap-2">
+                  {formik.values.images.map((image, index) => (
+                    <div key={index} className="relative">
+                      <img
+                        className="w-24 h-24 object-cover"
+                        src={image}
+                        alt="Foto do Restaurante"
+                      />
+                      <IconButton
+                        size="small"
+                        sx={{
+                          position: "absolute",
+                          top: "0",
+                          right: "0",
+                          outline: "none",
+                        }}
+                        onClick={() => handleRemoveImage(index)}
+                      >
+                        <Close sx={{ fontSize: "1rem" }} />
+                      </IconButton>
+                    </div>
+                  ))}
+                </div>
               </div>
+              {formik.touched.images && formik.errors.images && (
+                <Typography color="error" variant="caption">
+                  {formik.errors.images}
+                </Typography>
+              )}
             </Grid>
+
+            {/* --- Outros Campos com Validação --- */}
+            {/* 3. Adicionar error e helperText a cada TextField */}
             <Grid item xs={12}>
               <TextField
                 fullWidth
@@ -147,6 +182,8 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.name}
+                error={formik.touched.name && Boolean(formik.errors.name)}
+                helperText={formik.touched.name && formik.errors.name}
               />
             </Grid>
             <Grid item xs={12}>
@@ -159,6 +196,13 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.description}
+                error={
+                  formik.touched.description &&
+                  Boolean(formik.errors.description)
+                }
+                helperText={
+                  formik.touched.description && formik.errors.description
+                }
               />
             </Grid>
             <Grid item xs={12} md={6}>
@@ -171,6 +215,13 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.cuisineType}
+                error={
+                  formik.touched.cuisineType &&
+                  Boolean(formik.errors.cuisineType)
+                }
+                helperText={
+                  formik.touched.cuisineType && formik.errors.cuisineType
+                }
               />
             </Grid>
             <Grid item xs={12} md={6}>
@@ -183,6 +234,13 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.openingHours}
+                error={
+                  formik.touched.openingHours &&
+                  Boolean(formik.errors.openingHours)
+                }
+                helperText={
+                  formik.touched.openingHours && formik.errors.openingHours
+                }
               />
             </Grid>
             <Grid item xs={12}>
@@ -195,6 +253,13 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.streetAddress}
+                error={
+                  formik.touched.streetAddress &&
+                  Boolean(formik.errors.streetAddress)
+                }
+                helperText={
+                  formik.touched.streetAddress && formik.errors.streetAddress
+                }
               />
             </Grid>
             <Grid item xs={12} md={4}>
@@ -207,6 +272,13 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.stateProvince}
+                error={
+                  formik.touched.stateProvince &&
+                  Boolean(formik.errors.stateProvince)
+                }
+                helperText={
+                  formik.touched.stateProvince && formik.errors.stateProvince
+                }
               />
             </Grid>
             <Grid item xs={12} md={4}>
@@ -219,6 +291,12 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.postalCode}
+                error={
+                  formik.touched.postalCode && Boolean(formik.errors.postalCode)
+                }
+                helperText={
+                  formik.touched.postalCode && formik.errors.postalCode
+                }
               />
             </Grid>
             <Grid item xs={12} md={4}>
@@ -231,6 +309,8 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.city}
+                error={formik.touched.city && Boolean(formik.errors.city)}
+                helperText={formik.touched.city && formik.errors.city}
               />
             </Grid>
             <Grid item xs={12}>
@@ -243,6 +323,8 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.country}
+                error={formik.touched.country && Boolean(formik.errors.country)}
+                helperText={formik.touched.country && formik.errors.country}
               />
             </Grid>
             <Grid item xs={12} md={6}>
@@ -255,6 +337,8 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.email}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
               />
             </Grid>
             <Grid item xs={12} md={6}>
@@ -267,6 +351,8 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.mobile}
+                error={formik.touched.mobile && Boolean(formik.errors.mobile)}
+                helperText={formik.touched.mobile && formik.errors.mobile}
               />
             </Grid>
             <Grid item xs={12} md={4}>
@@ -279,6 +365,8 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.twitter}
+                error={formik.touched.twitter && Boolean(formik.errors.twitter)}
+                helperText={formik.touched.twitter && formik.errors.twitter}
               />
             </Grid>
             <Grid item xs={12} md={4}>
@@ -291,6 +379,10 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.instagram}
+                error={
+                  formik.touched.instagram && Boolean(formik.errors.instagram)
+                }
+                helperText={formik.touched.instagram && formik.errors.instagram}
               />
             </Grid>
             <Grid item xs={12} md={4}>
@@ -303,6 +395,10 @@ export const CreateRestaurantForm = () => {
                 variant="outlined"
                 onChange={formik.handleChange}
                 value={formik.values.facebook}
+                error={
+                  formik.touched.facebook && Boolean(formik.errors.facebook)
+                }
+                helperText={formik.touched.facebook && formik.errors.facebook}
               />
             </Grid>
           </Grid>
